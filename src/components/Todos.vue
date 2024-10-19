3437250a7ac6e44712c98c743442e2422afb66a0
Tito David Plasencia <tito.plasencia@htech.com>
2024-10-18 20:34:00 -0600

    added authenticator

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -25,7 +25,11 @@ function createTodo() {
     listTodos();
   });
 }
-    
+
+function deleteTodo(id: string) {
+  client.models.Todo.delete({ id })
+}
+
 // fetch todos when the component is mounted
  onMounted(() => {
   listTodos();
@@ -40,7 +44,9 @@ function createTodo() {
     <ul>
       <li 
         v-for="todo in todos" 
-        :key="todo.id">
+        :key="todo.id"
+        @click="deleteTodo(todo.id)"
+      >
         {{ todo.content }}
       </li>
     </ul>
